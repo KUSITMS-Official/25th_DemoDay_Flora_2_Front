9586a418064abc07976792bd2eec78742a9603a1
hidihyeonee <59243729+leejihyeon240@users.noreply.github.com>
2022-05-20 12:00:32 +0900

    dihyeonee

---
+++ app/src/main/java/com/example/flora/AfterSearchPage.java
@@ -0,0 +1,14 @@
+package com.example.flora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AfterSearchPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_after_search_page);
+    }
+}
