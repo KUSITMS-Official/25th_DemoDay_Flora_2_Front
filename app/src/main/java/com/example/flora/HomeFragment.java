47b530f71328f3f7be49e9540548e980d2c0aaeb
신혜승 <hyeseung2000@gmail.com>
2022-05-26 23:44:58 +0900

    [feat] flora 로고 추가

--- app/src/main/java/com/example/flora/HomeFragment.java
+++ app/src/main/java/com/example/flora/HomeFragment.java
@@ -430,6 +430,7 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
                 }
             });
             marker.setIcon(OverlayImage.fromResource(R.drawable.ic_map_pin_yellow));
+            marker.setWidth(16); marker.setHeight(19);
             activeMarkers.add(marker);
         }
     }
